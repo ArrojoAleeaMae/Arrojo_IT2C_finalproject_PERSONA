abee5e4c17141cde0074fd5a14729004da816ce4
Aleea Mae Arrojo <101234835+ArrojoAleeaMae@users.noreply.github.com>
2022-05-14 22:49:02 +0800

    initial files

---
+++ index.php
@@ -0,0 +1,149 @@
+<?php 
+//start the session function
+session_start();
+
+//create a pre defined username and password since we do not have database
+$username = "Aleea";
+$password = "mae";
+$title = " PERSONA";
+$caption = "Collect stories now! The more you read, the more things you will know!";
+
+//check the currenrt url for the redirections later
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+
+//condition to know if the button is clicked
+if (isset($_REQUEST['login_button']) === true){
+	//get the username and password from the form and compare to the predefined username ans password
+	//kapag mali agad yung username
+
+		if ($_REQUEST['form_username'] != $username) {
+			header("Location: ".$url_add."?notexist");
+
+		}
+		//tama ang username pero mali ang password
+		else if ($_REQUEST['form_username'] == $username && $_REQUEST['form_password'] != $password) {
+			header("Location: ".$url_add."?wrongpass");
+		}
+		//tama ang username at password
+		else if ($_REQUEST['form_username'] == $username && $_REQUEST['form_password'] == $password) {
+			header("Location: ".$url_add."?success");
+
+			//create session variables
+			$_SESSION['ses_username'] = $username;
+			$_SESSION['ses_password'] = $password;
+			$_SESSION['ses_title'] = $title;
+			$_SESSION['ses_caption'] = $caption;
+
+		}//end of correct username and password
+
+
+
+
+}//end of login button
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Persona Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body>
+		<header class=" bg-gradient text-black">
+            <div class="container px-4 text-center">
+                <img src="images/persona.png">
+            </div>
+    </header>
+
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-5">
+					<div class="login-wrap p-4 p-md-5">
+		      	<div class="icon d-flex align-items-center justify-content-center">
+		      		<span class="fa fa-user-o"></span>
+		      	</div>
+		      	<h3 class="text-center mb-4">Login To Open Persona</h3>
+						
+
+
+						<form method="POST" class="login-form">
+
+		      		<div class="form-group">
+
+		      			<?php
+		      			//this is the messaging
+		      			if (isset($_REQUEST['notexist'])===true) {
+		      					echo "<div class='alert alert-danger' role='alert'> Username does not exist... </div>";
+
+		      			}else if (isset($_REQUEST['wrongpass'])===true) {
+		      					echo "<div class='alert alert-warning' role='alert'> Incorrect Password... </div>";
+
+		      			}else if (isset($_REQUEST['success']) === true) {
+		      					echo "<div class='alert alert-success' role='alert'> Redirecting... </div>";
+		      					header("Refresh: 3; url=home.php");
+
+		      			}else if (isset($_REQUEST['logout'])===true) {
+		      					echo "<div class='alert alert-info' role='alert'> Thank You... </div>";
+
+		      			}else if (isset($_REQUEST['logfirst'])===true){
+		      					echo "<div class='alert alert-info' role='alert'> Please Login First. </div>";
+
+		      			}else if (isset($_SESSION['ses_username'])===true){
+										echo "<div class='alert alert-warning' role='alert'> You are still logged in. Please <a href ='home.php'> click here </a>to proceed. </div>";
+		      			}
+
+		      			
+		      			?>
+
+		      			<input type="text" class="form-control rounded-left" placeholder="Username" name="form_username" required>
+		      		</div>
+	            <div class="form-group d-flex">
+	              <input type="password" class="form-control rounded-left" placeholder="Password" name="form_password" required>
+	            </div>
+	            <div class="form-group d-md-flex">
+	            	<div class="w-50">
+	            		<label class="checkbox-wrap checkbox-primary">Remember Me
+									  <input type="checkbox" checked>
+									  <span class="checkmark"></span>
+									</label>
+								</div>
+								<div class="w-50 text-md-right">
+									<a href="#">Forgot Password</a>
+								</div>
+	            </div>
+	            <div class="form-group">
+	            	<button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="login_button">Open</button>
+	            </div>
+	          </form>
+
+
+
+
+	        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+  <!--<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script> -->
+
+	</body>
+</html>
+
